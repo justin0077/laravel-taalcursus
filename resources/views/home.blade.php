2cd7bc73e23b94239925f1eba016895c79a5b5af
justin0077 <justinkwaak@hotmail.com>
2016-10-07 11:57:54 +0200

    test
    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,28 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    U bent ingelogd
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Cursussen</div>
+
+                <div class="panel-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">Over ons</div>
+
+                <div class="panel-body">
+
                 </div>
             </div>
         </div>
